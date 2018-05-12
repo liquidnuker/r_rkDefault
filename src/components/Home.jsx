19d7cc58edaa5a3b70dee93709be89578d7cb991
administrator <liquidnuker@users.noreply.github.com>
2018-05-12 09:46:56 -0700

    added bs4grid

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 export default function Home(props) {
   return (
-    <div class="row">    
-      <div class="col-sm-12">
+    <div className="row no-gutters">    
+      <div className="col-sm-3">
         Home
+      </div>
+      <div className="col-sm-9">
+        39
       </div>
     </div>  
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import "./styles/main.scss";
 
 // router with route config
-import "./components/Home+Router.jsx";
+// import "./components/Home+Router.jsx";
 
 // default
-// import Home from "./components/Home.jsx";
-// ReactDOM.render(  
-//   <Home />,
-//   document.getElementById('root')
-// );
+import Home from "./components/Home.jsx";
+ReactDOM.render(  
+  <Home />,
+  document.getElementById('root')
+);
